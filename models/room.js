f257a789dc8e10fc8361979852ccf87b59646bdc
rjsah5676 <rjsah5676@gmail.com>
2021-05-20 19:59:12 +0900

    캠 필기를 통한 기능 선택

---
+++ models/room.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const roomSchema = new Schema({
+    hostId: {
+        type: String,
+        required: true,
+    },
+    roomId: {
+        type: String,
+        required: true,
+    },
+    participant:{
+        type:Number,
+        default:0,        
+    }
+});
+
+module.exports = mongoose.model('room', roomSchema);
