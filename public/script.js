aebf5b7d44d1d775661ec313e590e6151858d64f
rjsah5676 <rjsah5676@gmail.com>
2021-05-29 21:49:24 +0900

    프레임 조정

--- public/script.js
+++ public/script.js
@@ -278,42 +278,6 @@ function extractDraw() {
   }
   }
 }
-/*
-function rgb2hsv (r, g, b) {
-  let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
-  rabs = r / 255;
-  gabs = g / 255;
-  babs = b / 255;
-  v = Math.max(rabs, gabs, babs),
-  diff = v - Math.min(rabs, gabs, babs);
-  diffc = c => (v - c) / 6 / diff + 1 / 2;
-  percentRoundFn = num => Math.round(num * 100) / 100;
-  if (diff == 0) {
-      h = s = 0;
-  } else {
-      s = diff / v;
-      rr = diffc(rabs);
-      gg = diffc(gabs);
-      bb = diffc(babs);
-      if (rabs === v) {
-          h = bb - gg;
-      } else if (gabs === v) {
-          h = (1 / 3) + rr - bb;
-      } else if (babs === v) {
-          h = (2 / 3) + gg - rr;
-      }
-      if (h < 0) {
-          h += 1;
-      }else if (h > 1) {
-          h -= 1;
-      }
-  }
-  return {
-      h: Math.round(h * 360),
-      s: percentRoundFn(s * 100),
-      v: percentRoundFn(v * 100)
-  };
-}*/
 
 myPeer.on('open', id => { //피어 접속시 맨 처음 실행되는 피어 함수
   user_id = id
@@ -1194,7 +1158,7 @@ var height = window.innerHeight
 //---캔버스 코드 시작---
 
 var mainFrame = 20
-var camWriteFrame = 1000
+var camWriteFrame = 25
 
 function allLoaded() {
   var socket = io.connect()
